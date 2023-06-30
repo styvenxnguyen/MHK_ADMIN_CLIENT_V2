04d05175d8320bfa424cf8b1806c4d5beffc58ab
PhongThanh0907 <phong.thanh971997@gmail.com>
2023-06-30 16:26:20 +0700

    update code

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -6,11 +6,13 @@ const PRODUCT = {
   GET_ALL_BRAND: '/brand/get-all',
   GET_ALL_TYPE: '/type/get-all',
   GET_ALL_PURCHASE: '/product/import/get-all',
-  GET_DETAIL: (id: string) => `/product/get-by-id/${id}`
+  GET_DETAIL: (id: string) => `/product/get-by-id/${id}`,
+  GET_ALL_PRODUCT: '/product/import/get-all'
 }
 
 const ProductService = {
   getListProduct: () => axiosConfig.get(PRODUCT.GET_ALL),
+  getListProductV2: () => axiosConfig.get(PRODUCT.GET_ALL_PRODUCT),
   getListProductPurchase: () => axiosConfig.get(PRODUCT.GET_ALL_PURCHASE),
   getListProductType: () => axiosConfig.get(PRODUCT.GET_ALL_TYPE),
   getListProductBrand: () => axiosConfig.get(PRODUCT.GET_ALL_BRAND),
--- src/views/Products/Purchase_Orders/Create_Edit.tsx
+++ src/views/Products/Purchase_Orders/Create_Edit.tsx
@@ -22,7 +22,7 @@ import PageLoader from '~/components/Loader/PageLoader'
 import Error from '~/views/Errors'
 import { TagService } from '~/services/tag.service'
 import ProductService from '~/services/product.service'
-import { Product, ProductVariant } from '~/types/Product.type'
+import { ProductPurchase } from '~/types/Product.type'
 import { handleAlertConfirm } from '~/hooks/useAlertConfirm'
 import { ButtonLoading } from '~/components/Button/LoadingButton'
 import Swal from 'sweetalert2'
@@ -48,8 +48,6 @@ const CEPurchaseOrder = () => {
   const [optionsProduct, setOptionsProduct] = useState([])
   const [optionsBranch, setOptionsBranch] = useState([])
   const [optionsSupplier, setOptionsSupplier] = useState([])
-  const [selectedProduct, setSelectedProduct] = useState<Product>()
-  const [optionsProductVariant, setOptionsProductVariant] = useState([])
   const [idSelectedSupplier, setIdSelectedSupplier] = useState('')
   const [selectedStaff, setSelectedStaff] = useState<SelectProps>()
   const [selectedBranch, setSelectedBranch] = useState<SelectProps>()
@@ -58,9 +56,10 @@ const CEPurchaseOrder = () => {
   const [canEdit, setCanEdit] = useState(true)
   const [selectedTags, setSelectedTags] = useState<SelectProps[]>([])
   const loadingMessage = () => 'Đang tải dữ liệu...'
-  const [priceList, setPriceList] = useState<PricePolicy[]>([])
+  // const [priceList, setPriceList] = useState<PricePolicy[]>([])
   const [valueTags, setValueTags] = useState<string[]>([])
   const [newTags, setNewTags] = useState<any>()
+  const [productsList, setProductsList] = useState<any>([])
 
   const totalQuantity = productList.reduce((acc: number, item: any) => acc + parseInt(item.product_amount), 0)
   const totalAmount = productList.reduce((acc: number, item: any) => acc + item.product_amount * item.product_price, 0)
@@ -300,11 +299,43 @@ const CEPurchaseOrder = () => {
 
   const getProductList = useCallback(async () => {
     try {
-      const res = await ProductService.getListProduct()
+      const res = await ProductService.getListProductV2()
       const result = res.data.data
-      const options = result.map((product: Product) => ({
-        label: `${product.product_SKU} - ${product.product_name}`,
-        value: product.id
+      setProductsList(result.map((e: ProductPurchase) => e.product_variant))
+      const options = result.map((product: ProductPurchase) => ({
+        label: (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between'
+            }}
+          >
+            <span>{`${product.product_variant.sku} - ${product.product_variant.name}`}</span>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <span style={{ textAlign: 'end' }}>
+                Giá nhập:{' '}
+                <span style={{ color: 'black' }}>{formatCurrency(parseInt(product.product_variant.price_sell))}</span>
+              </span>
+              <div>
+                <span>
+                  Tồn:{' '}
+                  <span style={{ color: `${product.product_variant.amount.inStock === 0 ? 'red' : 'blue'}` }}>
+                    {product.product_variant.amount.inStock}
+                  </span>
+                </span>
+                {` `}|{` `}
+                <span>
+                  Có thể bán:{' '}
+                  <span style={{ color: `${product.product_variant.amount.inStock === 0 ? 'red' : 'blue'}` }}>
+                    {product.product_variant.amount.available_to_sell}
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        ),
+        value: product.product_variant.id
       }))
       setOptionsProduct(options)
     } catch (error) {
@@ -312,21 +343,21 @@ const CEPurchaseOrder = () => {
     }
   }, [])
 
-  const getProductDetail = useCallback(async (id: string) => {
-    setSelectedProduct(undefined)
-    try {
-      const res = await ProductService.getDetailProduct(id)
-      const dataProduct: any = res.data.data
-      setSelectedProduct(dataProduct)
-      const options = dataProduct.productVariants.map((variant: ProductVariant) => ({
-        label: variant.product_variant_name,
-        value: variant.id
-      }))
-      setOptionsProductVariant(options)
-    } catch (error) {
-      console.log(error)
-    }
-  }, [])
+  // const getProductDetail = useCallback(async (id: string) => {
+  //   setSelectedProduct(undefined)
+  //   try {
+  //     const res = await ProductService.getDetailProduct(id)
+  //     const dataProduct: any = res.data.data
+  //     setSelectedProduct(dataProduct)
+  //     const options = dataProduct.productVariants.map((variant: ProductVariant) => ({
+  //       label: variant.product_variant_name,
+  //       value: variant.id
+  //     }))
+  //     setOptionsProductVariant(options)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }, [])
 
   const getAgencyBranch = useCallback(async () => {
     try {
@@ -456,7 +487,7 @@ const CEPurchaseOrder = () => {
 
   const selectedProductNew = useCallback(
     (e: any) => {
-      const product = selectedProduct?.productVariants.find((item) => item.id === e.value)
+      const product = productsList?.find((item: { id: string }) => item.id === e.value)
       if (product) {
         setProductList([
           ...productList,
@@ -464,26 +495,26 @@ const CEPurchaseOrder = () => {
             order_product_item_id: product?.id,
             product_amount: 1,
             product_discount: 0,
-            product_price: 0,
+            product_price: product?.price_sell,
             product_unit: 'Cái',
-            product_variant_detail_SKU: product?.product_variant_SKU,
+            product_variant_detail_SKU: product?.sku,
             product_variant_detail_id: product?.id,
-            product_variant_detail_name: product.product_variant_name
+            product_variant_detail_name: product?.name
           }
         ])
       }
     },
-    [selectedProduct?.productVariants, productList]
+    [productsList, productList]
   )
 
-  const getListPrice = useCallback(async () => {
-    try {
-      const res = await PricePolicyService.getListPrice()
-      setPriceList(res.data.data)
-    } catch (error) {
-      console.log(error)
-    }
-  }, [])
+  // const getListPrice = useCallback(async () => {
+  //   try {
+  //     const res = await PricePolicyService.getListPrice()
+  //     setPriceList(res.data.data)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }, [])
 
   useEffect(() => {
     if (params.id) {
@@ -499,17 +530,7 @@ const CEPurchaseOrder = () => {
     getSupplierList()
     getProductList()
     getTagList()
-    getListPrice()
-  }, [
-    getPurchaseOrderDetail,
-    getStaffList,
-    getAgencyBranch,
-    getSupplierList,
-    getProductList,
-    getTagList,
-    getListPrice,
-    params.id
-  ])
+  }, [getPurchaseOrderDetail, getStaffList, getAgencyBranch, getSupplierList, getProductList, getTagList, params.id])
 
   useEffect(() => {
     TagService.getListTag().then((response) => {
@@ -766,22 +787,22 @@ const CEPurchaseOrder = () => {
                   className='mt-4'
                   options={optionsProduct}
                   onChange={(e: any) => {
-                    getProductDetail(e.value)
+                    selectedProductNew(e)
                   }}
                   placeholder={customPlaceholder('Product')}
                 />
               )}
 
-              {selectedProduct && (
+              {/* {selectedProduct && (
                 <Select
                   className='mt-4'
                   options={optionsProductVariant}
                   onChange={(e: any) => {
-                    selectedProductNew(e)
+                   
                   }}
                   placeholder='Chọn phiên bản sản phẩm'
                 />
-              )}
+              )} */}
             </Card.Header>
             <Card.Body>
               {productList.length === 0 ? (
